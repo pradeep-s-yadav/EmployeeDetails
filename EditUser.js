5370875f4357aaa01e2c82a1477473a403c87745
Pradeep Yadav <py78158@gmail.com>
2023-07-20 20:43:16 -0400

    Frontend using ReactJS

---
+++ EditUser.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
+export default function EditUser() {
+
+    let navigate=useNavigate()
+
+    const {id}= useParams()
+
+    const [user, setUser] = useState({
+        name:"",
+        username:"",
+        email:""
+    })
+
+    const{name, username,email}=user;
+
+    const onInputChange=(e)=>{
+
+        setUser({...user,[e.target.name]: e.target.value})
+
+    }
+
+    useEffect(()=>{
+        loadUser()
+    }, [])
+
+    const onSubmit=async(e) => {
+        e.preventDefault()
+        await axios.put(`http://localhost:8080/users/${id}`, user)
+        navigate("/")
+    }
+
+    const loadUser=async() =>{
+        const result = await axios.get(`http://localhost:8080/users/${id}`)
+        setUser(result.data)
+    }
+
+  return (
+    <div className='container'>
+        <div className='row'>
+            <div className='col-md-5 offset-md-4 border rounded p-4 mt-2 shadow'>
+                <h2 className='text-center m-4'>Edit User</h2>
+                <form onSubmit={(e)=>onSubmit(e)}>
+                <div className='mb-3'>
+                    <label htmlFor='Name' className='form-label'>Name</label>
+                    <input type="text" className='form-control' placeholder='Enter your name' 
+                    name='name' value={name} onChange={(e)=>onInputChange(e)}></input>    
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor='Username' className='form-label'>Username</label>
+                    <input type="text" className='form-control' placeholder='Enter your username' 
+                    name='username' value={username} onChange={(e)=>onInputChange(e)}></input>    
+                </div>
+                <div className='mb-3'>
+                    <label htmlFor='Email' className='form-label'>Email</label>
+                    <input type="text" className='form-control' placeholder='Enter your email' 
+                    name='email' value={email} onChange={(e)=>onInputChange(e)}></input>    
+                </div>
+                <button type="submit" className='btn btn-outline-primary'>Edit</button>
+                <Link className='btn btn-outline-danger mx-2' to="/">Cancel</Link>
+                </form>
+            </div>
+        </div>
+
+    </div>
+  )
+}
